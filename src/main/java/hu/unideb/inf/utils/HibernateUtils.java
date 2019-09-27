5a67d400a4e3d3620490c20d4050f7abb084f079
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-09-28 01:38:39 +0200

    Hibernate utils megirása.Beta verzio

--- src/main/java/hu/unideb/inf/utils/HibernateUtils.java
+++ src/main/java/hu/unideb/inf/utils/HibernateUtils.java
@@ -1,4 +1,45 @@
 package hu.unideb.inf.utils;
 
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+
 public class HibernateUtils {
+
+    private static SessionFactory factory= null;
+    private static Configuration conf;
+
+
+    private static  SessionFactory buildSessionFancotry(){
+        try {
+            conf = new Configuration(); {
+
+                conf.configure("hibernate.cfg.xml");
+
+                System.out.println("Configuration Load Secces...");
+                factory = conf.buildSessionFactory();
+
+                System.out.println("Sessionfactory Load Secces.. ");
+                return factory;
+            }
+
+        }
+        catch(Throwable ex){
+            ex.printStackTrace();
+
+            throw  new ExceptionInInitializerError(ex);
+        }
+    }
+
+
+    public static  SessionFactory getSessionFactory(){
+        if(factory ==null )
+
+            factory = buildSessionFancotry();
+        return factory;
+    }
+
 }
