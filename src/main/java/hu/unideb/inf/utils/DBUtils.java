81cb7d1676aa4b51139f77bf626866468c8aec8b
megvagyhadnagy01 <38715272+megvagyhadnagy01@users.noreply.github.com>
2019-09-28 01:40:08 +0200

    Dbutils megirása.Conetion kialakitására.

--- src/main/java/hu/unideb/inf/utils/DBUtils.java
+++ src/main/java/hu/unideb/inf/utils/DBUtils.java
@@ -1,4 +1,31 @@
 package hu.unideb.inf.utils;
 
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+
+
+
 public class DBUtils {
+    private static Transaction transaction;
+
+    public static Integer updateTable(String command) {
+        Query query = null;
+        try (Session session = HibernateUtils.getSessionFactory().openSession()) {
+            transaction = session.beginTransaction();
+            query = session.createQuery(command);
+            return query.executeUpdate();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+        }
+
+        return 0;
+    }
+
 }
