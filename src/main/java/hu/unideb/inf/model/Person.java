0bbd028ff8ce274c20ef65a3008c6b6b5c039abc
notusedusername <xnorbikax@gmail.com>
2019-09-27 23:07:59 +0200

    Backend funkciók impementálása - Tag entity

---
+++ src/main/java/hu/unideb/inf/model/Person.java
@@ -0,0 +1,72 @@
+package hu.unideb.inf.model;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import java.sql.Date;
+
+public class Person {
+
+    @Id
+    @Column
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private Date birthDate;
+
+    @Column
+    private String adress;
+
+    public Person(Long id, String firstName, String lastName, Date birthDate, String adress) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.birthDate = birthDate;
+        this.adress = adress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+}
