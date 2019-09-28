7fb632a859519958e9c38e03ec1b1586c388df4a
keserukristof <41549423+keserukristof@users.noreply.github.com>
2019-09-28 12:08:46 +0200

    Elkezdtem megírni az UpdatePersonController classt illetve a MainMenu.fxml fájlban a Tag Módosítása menüpontból eltávolítottam a disabled="true" beállítást

--- src/main/java/hu/unideb/inf/controller/UpdatePersonController.java
+++ src/main/java/hu/unideb/inf/controller/UpdatePersonController.java
@@ -1,4 +1,21 @@
 package hu.unideb.inf.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+import java.io.IOException;
+
+import static hu.unideb.inf.controller.utils.FXUtils.configScene;
+import static hu.unideb.inf.controller.utils.FXUtils.loadFxml;
+
 public class UpdatePersonController {
+    public void handleSelection(MouseEvent mouseEvent) {
+    }
+
+    public void handleMod(ActionEvent actionEvent) {
+    }
+
+    public void handleBack(ActionEvent actionEvent) throws IOException {
+        configScene(loadFxml("/fxml/MainMenu.fxml"));
+    }
 }
