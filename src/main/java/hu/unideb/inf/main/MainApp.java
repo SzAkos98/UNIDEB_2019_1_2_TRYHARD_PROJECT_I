0c8fa4cf6b141e8232f96bc8f42bd9af08bb4c1d
notusedusername <xnorbikax@gmail.com>
2019-09-29 16:01:44 +0200

    BackEnd funkciók implementálása - Főmenü DB initializálás on startup

--- src/main/java/hu/unideb/inf/main/MainApp.java
+++ src/main/java/hu/unideb/inf/main/MainApp.java
@@ -1,5 +1,6 @@
 package hu.unideb.inf.main;
 
+import hu.unideb.inf.utils.HibernateUtils;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import static hu.unideb.inf.controller.utils.FXUtils.*;
@@ -13,7 +14,7 @@ public class MainApp extends Application {
         MainApp.primaryStage = primaryStage;
         configScene(loadFxml("/fxml/MainMenu.fxml"));
         askConfirmationOnClose(primaryStage);
-
+        HibernateUtils.getSessionFactory();
     }
 
     public static void main(String[] args){
